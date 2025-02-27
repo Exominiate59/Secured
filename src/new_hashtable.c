ad63437e712546d6c912eec7c2e28c709fddd1f9
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:39:18 +0100

    feat(new_hashtable.c): added new_hashtable function

---
+++ src/new_hashtable.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-secured-antoine.rousselle
+** File description:
+** new_hashtable.c
+*/
+
+#include "hashtable.h"
+
+hashtable_t *new_hashtable(int (*hash)(char *, int), int len)
+{
+    hashtable_t *new_hashtable = malloc(sizeof(hashtable_t));
+
+    if (!new_hashtable || len <= 0 || !hash)
+        return 84;
+    new_hashtable->len = len;
+    new_hashtable->node = malloc(len * sizeof(hashnode_t));
+    if (!new_hashtable->node) {
+        free(new_hashtable);
+        return 84;
+    }
+    for (int i = 0; i < len; i++)
+        new_hashtable->node[i] = NULL;
+    return new_hashtable;
+}
