9a7142f12f22f9ecc80b893bfab2e288fd0a3df2
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:40:27 +0100

    feat(lib/my/mini_printf.c): added mini_printf function

---
+++ lib/my/mini_printf.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2024
+** mini_printf
+** File description:
+** do the flag s, c, d and i of printf
+*/
+
+#include "my.h"
+
+void verif_spec(char format, va_list arg)
+{
+    if (format == 'd' || format == 'i')
+        my_put_nbr(va_arg(arg, int));
+    if (format == 's')
+        my_putstr(va_arg(arg, char *));
+    if (format == 'c')
+        my_putchar(va_arg(arg, int));
+    if (format == '%')
+        my_putchar('%');
+    return;
+}
+
+int mini_printf(const char *format, ...)
+{
+    va_list arg;
+
+    va_start(arg, format);
+    for (int i = 0; format[i]; i++) {
+        if (format[i] == '%') {
+            verif_spec(format[i + 1], arg);
+            i++;
+        } else
+            my_putchar(format[i]);
+    }
+    va_end(arg);
+    return 0;
+}
