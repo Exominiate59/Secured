b977736bd43cf8d9d477bfd9ecf5e9f3ecca94fa
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:39:02 +0100

    feat(main.c): add void main

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-secured-antoine.rousselle
+** File description:
+** main.c
+*/
+
+#include "hashtable.h"
+#include "my.h"
+
+int main(void)
+{
+    return 0;
+}
