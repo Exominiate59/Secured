69e822fb347428a96686c599a9545c8b80acad80
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:40:16 +0100

    feat(hash.c): changed hash because he causes some bugs

--- src/hash.c
+++ src/hash.c
@@ -7,34 +7,17 @@
 
 #include "hashtable.h"
 
-void generate_fibonacci(int fib[], int n)
-{
-    fib[1] = 1;
-    for (int i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-}
-
 int hash(char *key, int len)
 {
-    const int constant = 358132134;
-    int fibonacci_base[20];
-    unsigned long long total = 0;
-    unsigned long long transformed = 0;
-    unsigned long long int mixed_total = 0;
-    int ascii_val = 0;
-    int position = 0;
+    if (!key || len <= 0)
+        return 84;
 
-    generate_fibonacci(fibonacci_base, 20);
-    for (int i = 0; key[i] != '\0'; i++) {
-        ascii_val = (int)key[i];
-        position = i + 1;
-        transformed = ascii_val;
-        for (int j = 0; j < 6; j++)
-            transformed *= fibonacci_base[(position + j - 1) % 20];
-        transformed += constant;
-        total += transformed;
+    unsigned long hash_value = 0;
+
+    for (int i = 0; key[i]; i++) {
+        hash_value = (hash_value * 31) + key[i];
+        hash_value ^= (hash_value >> 15);
     }
-    mixed_total = (total >> 16);
-    return mixed_total % (int)my_compute_power_it(10, len);
+    return (int)(hash_value % 10000000);
 }
+
