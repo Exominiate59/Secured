d86ba23880e399821b42559b531e23010fe977be
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:41:08 +0100

    chore(ht_search.c): removed too strict error handling

--- src/ht_search.c
+++ src/ht_search.c
@@ -13,18 +13,14 @@ char *ht_search(hashtable_t *ht, char *key)
     int index = 0;
     hashnode_t *current = NULL;
 
-    if (!ht || !key || !ht->node || ht->len == 0)
+    if (ht == NULL || key == NULL)
         return NULL;
     hashed_key = hash(key, ht->len);
     index = hashed_key % ht->len;
-    if (index < 0 || index >= ht->len || hashed_key < 0)
-        return NULL;
     current = ht->node[index];
     while (current) {
-        if (!current->hash_value)
-            return NULL;
-        if (hashed_key == current->hash_value)
-                return current->value;
+        if (my_strcmp(current->key, key) == 0)
+            return current->value;
         current = current->next;
     }
     return NULL;
