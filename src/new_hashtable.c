333ad7339b8a4bfd3e012ecf43915eec31c507df
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:39:48 +0100

    chore(new_hashtable.c): changed return value

--- src/new_hashtable.c
+++ src/new_hashtable.c
@@ -12,13 +12,13 @@ hashtable_t *new_hashtable(int (*hash)(char *, int), int len)
     hashtable_t *new_hashtable = malloc(sizeof(hashtable_t));
 
     if (!new_hashtable || len <= 0 || !hash)
-        return 84;
+        return NULL;
     new_hashtable->len = len;
     new_hashtable->node = malloc(len * sizeof(hashnode_t));
     new_hashtable->hash = hash;
     if (!new_hashtable->node) {
         free(new_hashtable);
-        return 84;
+        return NULL;
     }
     for (int i = 0; i < len; i++)
         new_hashtable->node[i] = NULL;
