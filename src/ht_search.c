7a7ae06cc96f537e449f8f6fbfe52d69a3397972
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:40:17 +0100

    feat(ht_search.c): added function that search the value

---
+++ src/ht_search.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LIL-1-1-secured-antoine.rousselle
+** File description:
+** ht_search.c
+*/
+
+#include "hashtable.h"
+
+char *ht_search(hashtable_t *ht, char *key)
+{
+     int hash_value = 0;
+    int index = 0;
+    hashnode_t *current = NULL;
+
+    if (!ht || !key)
+        return NULL;
+    hash_value = hash(key, ht->len);
+    index = hash_value % ht->len;
+    current = ht->node[index];
+    while (current) {
+        if (hash_value == current->hash_value
+            && my_strcmp(current->key, key) == 0)
+                return current->value;
+        current = current->next;
+    }
+    return NULL;
+}
