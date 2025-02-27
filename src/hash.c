cd1aa145403612520b4c61c831210977e61adad3
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-02-27 09:41:00 +0100

    chore(hash.c): fixed hashing

--- src/hash.c
+++ src/hash.c
@@ -12,10 +12,10 @@ int hash(char *key, int len)
     unsigned long hash_value = 0;
 
     if (!key || len <= 0)
-        return 84;
+        return -1;
     for (int i = 0; key[i]; i++) {
         hash_value = (hash_value * 31) + key[i];
         hash_value ^= (hash_value >> 15);
     }
-    return (int)(hash_value % 10000000);
+    return (int)(hash_value % len);
 }
